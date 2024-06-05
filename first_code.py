5a9ae2f6fc04c916b3d0ded658e3328865c96734
shubhangi <shubhangi1152005@gmail.com>
2024-06-05 11:52:41 +0530

    added

--- first_code.py
+++ first_code.py
@@ -3,4 +3,6 @@ import keyword
 print(keyword.kwlist)
 print(len(keyword.kwlist)) #calculate the lenght of keyword list
 
-input
+#INPUT THEN TYPE OF DATA TYPE SYNTAX
+#OOPS--->>> every type in python is an object of a class
+#TYPE CASTING ---> int(),str(),float()
