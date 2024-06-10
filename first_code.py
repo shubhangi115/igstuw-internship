0df6c14d53cb1449084a3b7270081f94eacb0aec
shubhangi <shubhangi1152005@gmail.com>
2024-06-10 13:42:19 +0530

    added

--- first_code.py
+++ first_code.py
@@ -168,13 +168,232 @@
 # str1 ="Hello all"
 # print("The no. of characters in string are :",len(str1))
 
-d = {'a': 1, 'b': 2}
-print(d.update({'b': 3, 'c': 4}))
+# list 
+#creating list
+# list1=[1,2,3,4,"abc",True,11.89,4+5j]
+# print("the list is: ", list1)
 
+# #how to create a blank list
+# list2=[] #empty list
+# print(list2)
 
+#since python is object oriented it has everything in form of object
+#list here in pyhton is also a class. a list object can be also instantiate by list class
+#construcyor by invoking list()
 
+# str1= "hello All"
+# # to convert this directly to list we can invoke list()
+# new_list=list(str1)
+# print("the newley formed list is: ",new_list)
 
+#properties of list:
+""" 1. ordered 
+    2. mutable
+    3. allows repeated values i.e. duplicates"""
 
+# 1. ordered --->> each element of list has an index position atteched
 
+# list1=[1,2,3,"hello",11.4,4+5j]
+#start access from left to right
+# print("first ", list1[0])
+# print("first ", list1[1])
+# print("first ", list1[2])
+# print("first ", list1[3])
+# print("first ", list1[4])
+# print("first ", list1[5])
 
+#slice and water negative-slice a list
+# list1= [1,2,3,"hello",11.4,4+5j]
+# #slice list from index 1 to 6
+# print("slice from index 1 to 6 ", list1[1:6])
+# print("negative slice from last position to second element ", list1[1:-1])
+# print(len(list1))
+# print(len(list1)/2)
 
+#2 Mutable : You an modify elements, add and remove elements from list
+
+# list1=[1,2,3,"abc",11.5,4+5j]
+# print("original list; ", list1)
+# append() ---->> to add a new element at the end of the list 
+# list1.append("new value")
+# print(" the appended list: ",list1)
+
+# insert ----->> to add element to a specific position
+# list1=[1,2,3,"abc",11.5,4+5j]
+# print("original: ",list1)
+# list1.insert(3,20) # insert(index, value to be inserted)
+# print("updated list: ", list1)
+
+# extend() ----->> you can add new values form another list into original list
+# list1=[1,2,3,"abc",11.5,4+5j]
+# print("original: ",list1)
+# list2=[7,8,9,"go"]
+# list1.extend(list2)
+# print("updated list", list1)
+# print("list 2 values: ", list2)
+
+#list concatenate
+# list1=[1,2,3,"abc",11.5,4+5j]
+# list2=[7,8,9,"go"]
+# print("list concatenation: ",list1+list2)
+
+# 3. list repetation
+# print("list repeate: ", list1*3)
+
+#operators common for sequence: +,*,[:]
+
+# make modification in list
+# list1=[1,2,3,"abc",11.5,4+5j]
+# print("original list: ",list1)
+#update the third element of list 3 --->> 30
+# list1[2]= 30
+# print("updated: ", list1)
+# list1[2:5]=[30,"hello", "byee",11] # no error when elements are added more than the index number
+# print("updated list: ",list1)
+
+# remove() ---->>> Remove elements from list
+# list1=[1,2,3,"abc",11.5,4+5j,"abc"]
+# print("original list: ",list1)
+
+# list1.remove("abc")
+# print("updated list: ",list1)
+# list1.remove("abc")
+# print("updated list: ",list1)
+
+# count() ---->> count occurence of elemets
+# list1=[1,2,3,"abc",11.5,4+5j,"abc"]
+# print("original list: ",list1)
+# print("count 'abc': ",list1.count("abc"))
+
+#sort a list
+# print("the sorted : ",list1.sort())
+#sorting works with same dataypes
+
+# list1=[1,2,4,1,3,2,9,0,880]
+# list1.sort() #in ascending
+# print("the sorted : ",list1)
+
+#sort in descending or reverse 
+# list1.sort(reverse=True)
+# print("the sorted : ",list1)
+# #for string also
+ # copy() ---->> create a copy of list 
+# list1=[1,2,4,1,3,2,9,0,880]
+# print("original list: ",list1)
+# newlist=list1.copy()
+# print("new list: ",newlist)
+
+#to check list is same or not
+#print(list1==newlist)
+
+# TUPLE -----> it is a sequence like list with some differences
+# 1. list is mutable but tuple is immutable
+# 2. list starts with [] while tuple starts with ()
+# 3. there is a fairly good number of methods in list while only 2 methods for tuple
+
+# creating a tuple
+# tup1=(1,2,3,4,"hello",11.44, True,False,3+3j)
+# print("the original tuple: ",tup1)
+# we have a tuple constructor tuple()
+# newtup=tuple(("hello","bye",1,2,3,22.44))
+# print("new tuple ", newtup)
+# list1=["hello","bye",1,2,3,22.44]
+# newtup=tuple(list1)
+# print("new tuple ", newtup)
+# tup1=(1,2,3,"hello",True,1,2,3)
+# list1=list(tup1)
+# print("list from tuple: ",list1)
+# str1="hello"
+# newtup=tuple(str1)
+# print("tuple from string: ",newtup)
+
+# empty or blank tuple
+# tup1=()
+# print("the tup1 is : ", tup1,"type of tup1 ", type(tup1))
+
+# create a tuple with only single value
+# tup1=("hello")   # interpreter will consider this tuple as string as it is a single value tuple and without a comma it will consider a string......but using comma after that it will generate tuple
+# print("the tup1 is : ", tup1,"type of tup1 ", type(tup1))
+# tup1=(25)
+# print("the tup1 is : ", tup1,"type of tup1 ", type(tup1))
+# tup1=("hello",)
+# print("the tup1 is : ", tup1,"type of tup1 ", type(tup1))
+# tup1=(25,)
+# print("the tup1 is : ", tup1,"type of tup1 ", type(tup1))
+# singleton tuple ---->> single value tuple(need to put comma after value)
+# list=[22] # n need to add comma in list
+# print("the list ",list,"type of list ", type(list))
+
+#default datatype of python is tuple
+# val=1,2,3,5,"go"
+# print("the values are ",val,"type of list ", type(val))
+
+# properties for tuples
+""" 1. ordered
+    2. immutable
+    3. allows duplicate values"""
+
+# 1.orrdered
+# tup1=(1,2,3,4,"hello",11.44, True,False,3+3j)
+# print("the original tuple: ",tup1)
+# print("the first element: ", tup1[0])
+# print("the first element: ", tup1[1])
+# print("the first element: ", tup1[2])
+# print("the first element: ", tup1[3])
+# slicing just like list
+
+# 2. immutable
+# list1=[1,2,3,"hello",11.55]
+# print("original list: ",list1)
+# tup1=(1,2,3,"hello",11.55)
+# print("original tup1: ",tup1)
+#try to modify 3---->>> in both list1 and tup1
+# list1[2]=30 
+# tup1[2]=30  # immutability
+# print("updated list: ",list1)
+# print("updated tuple: ",tup1)
+# security stored data are used in tuple(you dont want data to be changed even unintentionally)
+
+# to find the number or count of lements in tuple
+# len()
+# tup1=(1,2,3,"hello",11.55)
+# print("original tup1: ",tup1)
+# print("lenght ", len(tup1))
+
+#check for membership in tuple 
+#check if "hello" exists in tuple
+# print('hello in tuple ',"hello" in tup1)
+# print('Hello in tuple ',"Hello" in tup1)
+# print('hello in tuple ',"hello" not in tup1)
+
+# no direct removal of elements allowed in tuple
+# however you can remove tuple in one go
+# del property
+# del tup1
+# print(tup1)  #this line will show error as tup 1 is removed in line 372
+
+# Two method in tuple;
+# 1. count(): count the number of occurence number of times
+# 2. index(): search and find index of an elemnt otherwise throw exeption
+# tup1=(1,2,3,"hello",11.55,"hello","Hello")
+# print("original tup1: ",tup1)
+# print("count of hello: ",tup1.count("hello"))
+# print("find if 'hello' is present: ", tup1.index("hello"))  # searches for first occurence index only
+
+# Set
+# properties
+# set begin with {} braces
+# it does not allow duplicates
+# constructor call set()
+# unordered
+# no slicing and indexing
+
+# creating set()
+set1={1,2,3,True,11.55,"hello",4+6j,1,1,1,2,2}  
+print("set: ",set1)
+
+#empty set
+set2=set()
+print("the empty set is: ",set2,"type of set: ",type(set2))
+set3={}
+print("the empty set is: ",set2,"type of set: ",type(set3))
